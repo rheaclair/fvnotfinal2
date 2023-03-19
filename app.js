942386f51852548a5de3d0b3a78d0fbbe3662210
rheaclair <120297125+rheaclair@users.noreply.github.com>
2023-03-19 08:31:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+let email=document.getElementById("txtEmail");
+let pwd=document.getElementById("txtPwd");
+let conPwd=document.getElementById("txtConPwd");
+let fn=document.getElementById("txtFirstname");
+let form=document.querySelector("form");
+
+function validateInput(){
+    
+    if(email.value.trim()===""){
+        onError(email,"Please enter your email address");
+    }else{
+        if(!isValidEmail(email.value.trim())){
+            onError(email,"Email is not valid");
+        }else{
+            onSuccess(email);
+        }
+    }
+
+    //password
+    if(pwd.value.trim()===""){
+        onError(pwd,"Please enter your password");
+     }else{
+         onSuccess(pwd);
+     }
+     if(conPwd.value.trim()===""){
+        onError(conPwd,"Please enter your password one more time");
+     }else{
+         if(pwd.value.trim()!==conPwd.value.trim()){
+            onError(conPwd,"Password & Confirm password not matching");
+         }
+         else
+         onSuccess(conPwd);
+     }
+
+        if(fn.value.trim()===""){
+        onError(fn,"Please enter your first name");
+    }else{
+            onSuccess(fn);
+        }
+
+}
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+});
+
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="hidden"; 
+    parent.classList.remove("error");
+    parent.classList.add("success");  
+}
+function onError(input,message){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="visible";
+    messageEle.innerText=message;  
+    parent.classList.add("error");
+    parent.classList.remove("success");
+
+}
+
+function isValidEmail(email){
+   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
